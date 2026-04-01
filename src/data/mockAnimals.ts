72f13b6bb56e2e741ff378a3055e4273e4660fa5
Dennis.C <dennis.cederqvist@chasacademy.se>
2026-04-01 12:17:33 +0200

    added mockAnimals.ts and Animal.ts as a baseline for the rest of the cards

---
+++ src/data/mockAnimals.ts
@@ -0,0 +1,83 @@
+import type { Animal } from "../types/Animal";
+
+export const mockAnimals: Animal[] = [
+  {
+    id: "1",
+    type: "Dog",
+    breed: "Labrador",
+    image: "https://placehold.co/200x200",
+    name: "Kalle",
+    age: 3,
+    keyTraits: "Busig",
+  },
+
+  {
+    id: "2",
+    type: "Dog",
+    breed: "Bulldog",
+    image: "https://placehold.co/200x200",
+    name: "Doris",
+    age: 3,
+    keyTraits: "Lat",
+  },
+
+  {
+    id: "3",
+    type: "Cat",
+    breed: "Norsk Skogskatt",
+    image: "https://placehold.co/200x200",
+    name: "Simba",
+    age: 3,
+    keyTraits: "Självständig",
+  },
+
+  {
+    id: "4",
+    type: "Cat",
+    breed: "Maine Coon",
+    image: "https://placehold.co/200x200",
+    name: "Mjausalot",
+    age: 3,
+    keyTraits: "Blyg",
+  },
+
+  {
+    id: "5",
+    type: "Orm",
+    breed: "Kungspyton",
+    image: "https://placehold.co/200x200",
+    name: "Nyx",
+    age: 3,
+    keyTraits: "Kelig",
+  },
+
+  {
+    id: "6",
+    type: "Orm",
+    breed: "Mjölksnok",
+    image: "https://placehold.co/200x200",
+    name: "Wiggles",
+    age: 3,
+    keyTraits: "Lugn",
+  },
+
+  {
+    id: "7",
+    type: "Fågel",
+    breed: "Grå jako",
+    image: "https://placehold.co/200x200",
+    name: "Merlin",
+    age: 3,
+    keyTraits: "Pratglad",
+  },
+
+  {
+    id: "8",
+    type: "Fågel",
+    breed: "Kakadua",
+    image: "https://placehold.co/200x200",
+    name: "Merlin",
+    age: 3,
+    keyTraits: "Skygg",
+  },
+];
---
+++ src/types/animal.ts
@@ -0,0 +1,9 @@
+export type Animal = {
+  id: string;
+  type: string;
+  breed: string;
+  image: string;
+  name: string;
+  age: number;
+  keyTraits: string;
+};
