98991378889db285e8d2c7c00c26814a815c009e
Lindsteez <lindgren.axel@hotmail.com>
2026-04-01 10:29:46 +0200

    added some styling and text inside the picture

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,8 +6,9 @@ function App() {
 
   return (
  
- <><Footer />
+ <>
     <Hero />
+    <Footer />
  </>
   )
 }
--- src/components/ui/hero/Hero.tsx
+++ src/components/ui/hero/Hero.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import hundåkatt from '../../../assets/hundåkatt.png';
+import style from './hero.module.css';
 
 export default function Hero() {
     return (
-        <div className="hero">
-            <h1 className="hero-title">Hitta din perfekta match</h1>
-            <p className="hero-description">Sök och finn din nya vänn idag</p>
-            <img src={hundåkatt} alt="Hero Image with picture of dog and cat" />
+        <div className={style.hero}>
+            <div className={style.imageWrapper}>
+                <img className={style.img} src={hundåkatt} alt="Hero Image with picture of dog and cat" />
+                <div className={style.textOverlay}>
+                    <h1 className={style.title}>Hitta din perfekta match</h1>
+                    <p className={style.description}>Sök och finn din nya vän idag</p>
+                </div>
+            </div>
         </div>
     );
 }
