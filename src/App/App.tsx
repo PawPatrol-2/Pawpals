4cdd02cbb6ec64c2006ce8d1db3092aaa9996dcd
JohannesFolkesson <johannes.folkesson@chasacademy.se>
2026-04-01 10:34:55 +0200

    Routes

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,16 @@
+import Navbar from '../components/ui/Navbar/Navbar'
 import './App.css'
+import AppRoutes from './Routes'
 
 function App() {
 
 
   return (
  
- <></>
+ <>
+   <Navbar />
+   <AppRoutes />
+ </>
  
   )
 }
