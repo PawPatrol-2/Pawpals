445e9a3915a36c934198ef817d3b1af69069a98d
JohannesFolkesson <johannes.folkesson@chasacademy.se>
2026-04-02 10:52:08 +0200

    .

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,19 +2,9 @@ import Hero from "../components/ui/hero/Hero";
 import Adoption from "../components/Adoption/adoption";
 
 export default function HomePage() {
-<<<<<<< HEAD
     return(
         <main>
             <Hero />
         </main>
     )
-=======
-  return (
-    <main>
-      <Hero />
-      <h2>Välkommen till PawPals</h2>
-      <p>Hitta ditt nya husdjur </p>
-    </main>
-  );
->>>>>>> 8fa40c887075189592b0935fc797003a34b0f903
 }
