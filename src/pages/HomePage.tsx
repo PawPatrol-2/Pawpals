4554b34ae9fbc360a854b000aaa367bbdec5c1a3
JohannesFolkesson <johannes.folkesson@chasacademy.se>
2026-04-02 09:57:53 +0200

    JIO

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,9 @@
+import Hero from '../components/ui/hero/Hero';
+
 export default function HomePage() {
     return(
         <main>
+            <Hero />
             <h2>Välkommen till PawPals</h2>
             <p>Hitta ditt nya husdjur </p>
         </main>
--- src/pages/LoginPage.tsx
+++
@@ -1,8 +0,0 @@
-export default function LoginPage() {
-    return (
-        <main>
-            <h2>Logga in</h2>
-            <p>Logga in på ditt konto</p>
-        </main>
-    )
-}
