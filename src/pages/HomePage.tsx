413b63a1a819aa2ab346a686399f478c03c97aa4
bjorkmanemelie <bjorkmanemelie@hotmail.com>
2026-04-02 09:26:52 +0200

    wokring on adoption section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,10 @@
+import Adoption from "../components/Adoption/adoption";
+
 export default function HomePage() {
-    return(
-        <main>
-            <h2>Välkommen till PawPals</h2>
-            <p>Hitta ditt nya husdjur </p>
-            
-        </main>
-    )
+  return (
+    <main>
+      <h2>Välkommen till PawPals</h2>
+      <p>Hitta ditt nya husdjur </p>
+    </main>
+  );
 }
