aa875b8f90214306a4ac31d018b57ace99d89fe3
JohannesFolkesson <johannes.folkesson@chasacademy.se>
2026-04-02 10:44:22 +0200

    Uppdatering, bort med placeholders

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,8 +6,6 @@ export default function HomePage() {
     return(
         <main>
             <Hero />
-            <h2>Välkommen till PawPals</h2>
-            <p>Hitta ditt nya husdjur </p>
         </main>
     )
 }
