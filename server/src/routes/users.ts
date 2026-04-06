97a32a24ddf0159fab243a49f6018fc8108900f9
JohannesFolkesson <johannes.folkesson@chasacademy.se>
2026-04-06 16:06:46 +0200

    Logga in backend + styling

---
+++ server/src/routes/users.ts
@@ -0,0 +1,13 @@
+// Route-definition för användare
+import express from 'express';
+const router = express.Router();
+import { loginUser } from '../controllers/userController';
+import { registerUser } from '../controllers/userController'
+
+
+router.post('/login', loginUser)
+router.post('/register', registerUser)
+
+
+
+export default router;
--- src/App/Providers.tsx
+++ src/App/Providers.tsx
@@ -1,10 +1,19 @@
 import { type ReactNode } from "react";
 import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "../context/UserContext";
 
 interface ProvidersProps {
   children: ReactNode;
 }
 
 export default function Providers({ children }: ProvidersProps) {
-  return <BrowserRouter>{children}</BrowserRouter>
+  return (
+  <BrowserRouter>
+  <UserProvider>
+
+  {children}
+  
+  </UserProvider>
+  </BrowserRouter>
+  )
 }
