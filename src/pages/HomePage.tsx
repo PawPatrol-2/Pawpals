30e0c36d00fc1857f981eb532892f0aa5e865339
Lindsteez <lindgren.axel@hotmail.com>
2026-04-07 12:08:13 +0200

    added search functionallity for hero

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,35 @@
+import { useMemo, useState } from "react";
 import Hero from "../components/ui/hero/Hero";
 import AnimalGrid from "../components/AnimalGrid/AnimalGrid";
 import { mockAnimals } from "../data/mockAnimals";
 import Adoption from "../components/Adoption/adoption";
+// import { mock } from "node:test";
 
 export default function HomePage() {
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredAnimals = useMemo(() => {
+    const normalizedSearchTerm = searchTerm.trim().toLowerCase();
+    if (!normalizedSearchTerm) {
+      return mockAnimals;
+    }
+
+    return mockAnimals.filter((animal) => {
+      return (
+        animal.type.toLowerCase().includes(normalizedSearchTerm) ||
+        animal.breed.toLowerCase().includes(normalizedSearchTerm) ||
+        animal.name.toLowerCase().includes(normalizedSearchTerm) ||
+        animal.keyTraits.toLowerCase().includes(normalizedSearchTerm) ||
+        animal.age.toString().includes(normalizedSearchTerm) 
+      );
+    });
+  }, [searchTerm]);
+
   return (
     <main>
-      <Hero />
-      <AnimalGrid animals={mockAnimals} />
+      <Hero onSearch={setSearchTerm} />
+      <AnimalGrid animals={filteredAnimals} />
       <h2>Välkommen till PawPals</h2>
       <p>Hitta ditt nya husdjur </p>
       <Adoption />
