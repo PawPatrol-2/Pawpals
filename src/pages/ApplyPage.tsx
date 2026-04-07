7b2a31013f7c2d060980535d6e55d6e32237ee53
bjorkmanemelie <bjorkmanemelie@hotmail.com>
2026-04-07 19:59:33 +0200

    ApplyPage with houseing type form field

--- src/pages/ApplyPage.tsx
+++ src/pages/ApplyPage.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 interface ApplicationForm {
   housingType: string;
   housingSize: number;
@@ -8,10 +10,30 @@ interface ApplicationForm {
   gdprConsent: boolean;
 }
 
+
 const ApplyPage = () => {
+  const [formData, setFormData] = useState<ApplicationForm>({
+    housingType: "",
+    housingSize: 0,
+    hasAnimalExperience: false,
+    hasChildren: false,
+    hasAllergies: false,
+    motivation: "",
+    gdprConsent: false,
+  });
   return (
     <main>
-      <h1>Ansök om adoption</h1>
+        <h1>Ansök om adoption</h1>
+      <form>
+        <label>Boendetyp</label>
+          <select className="Ansökan" value={formData.housingType} onChange={(e) => setFormData({ ...formData, housingType: e.target.value })} 
+          >
+          <option value="">Välj boendetyp</option>
+          <option value="lagenhet">Lägenhet</option>
+          <option value="villa">Villa</option>
+          <option value="radhus">Radhus</option>
+          </select>
+      </form>
     </main>
   );
 };
