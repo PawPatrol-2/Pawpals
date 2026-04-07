2928c2c91f7c4b9b05bcd6aee6aa542565034e6f
JohannesFolkesson <johannes.folkesson@chasacademy.se>
2026-04-07 15:05:16 +0200

    Email + username update

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,13 +1,15 @@
-import mongoose, { Schema, Document} from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface User extends Document {
+    email: string;
     username: string;
     password: string;
 }
 
 const UserSchema: Schema = new Schema({
-    username: { type: String, required: true, unique: true},
-    password: { type: String, required: true}
-})
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+});
 
-export default mongoose.model<User>('User', UserSchema)
+export default mongoose.model<User>("User", UserSchema);
