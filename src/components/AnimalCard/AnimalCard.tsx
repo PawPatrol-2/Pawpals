bdbb6e6d00b2979976daba51e913d732a569faf6
Dennis.C <dennis.cederqvist@chasacademy.se>
2026-04-08 09:57:04 +0200

    added favoirte hearts

--- src/components/AnimalCard/AnimalCard.tsx
+++ src/components/AnimalCard/AnimalCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { Animal } from "../../types/Animal";
 import styles from "./AnimalCard.module.css";
 
@@ -6,13 +7,28 @@ type AnimalCardProps = {
 };
 
 function AnimalCard({ animal }: AnimalCardProps) {
+  const [isFavorite, setIsFavorite] = useState(false);
+
   return (
     <article className={styles.card}>
       <img className={styles.image} src={animal.image} alt={animal.name} />
 
       <div className={styles.content}>
         <h2 className={styles.name}>{animal.name}</h2>
-        <p className={styles.meta}>{animal.age} år • {animal.keyTraits} • 🐾</p>
+
+        <p className={styles.meta}>
+          {animal.age} år • {animal.keyTraits}
+        </p>
+
+        <button
+          className={`${styles.heart} ${isFavorite ? styles.active : ""}`}
+          onClick={(e) => {
+            e.stopPropagation();
+            setIsFavorite((prev) => !prev);
+          }}
+        >
+          ♥
+        </button>
       </div>
     </article>
   );
