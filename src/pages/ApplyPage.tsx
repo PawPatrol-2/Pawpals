6d4c71805866632e0c927e678e167acda113642f
bjorkmanemelie <bjorkmanemelie@hotmail.com>
2026-04-08 10:07:51 +0200

    froms css + tsx styles names

--- src/pages/ApplyPage.tsx
+++ src/pages/ApplyPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styles from "./ApplyPage.module.css";
 
 interface ApplicationForm {
   housingType: string;
@@ -23,10 +24,10 @@ const ApplyPage = () => {
   return (
     <main>
       <h1>Ansök om adoption</h1>
-      <form>
-        <label className="housing-type">Boendetyp</label>
+      <form className={styles.form}>
+        <label className={styles.housingType}>Boendetyp</label>
         <select
-          className="form-housetype"
+          className={styles.formHousetype}
           value={formData.housingType}
           onChange={(e) =>
             setFormData({ ...formData, housingType: e.target.value })
@@ -37,15 +38,40 @@ const ApplyPage = () => {
           <option value="villa">Villa</option>
           <option value="radhus">Radhus</option>
         </select>
-        <label className="label-name">Storlek på bostad (kvm)</label>
+        <label className={styles.labelName}>Storlek på bostad (kvm)</label>
         <input
           type="number"
-          className="form-input"
+          className={styles.formInput}
           value={formData.housingSize}
           onChange={(e) =>
             setFormData({ ...formData, housingSize: Number(e.target.value) })
           }
         ></input>
+        <label className={styles.formLabel}>Har du erfarenhet av djur?</label>
+        <div className="radio-group">
+          <label>
+            <input
+              type="radio"
+              name="hasAnimalExperience"
+              checked={formData.hasAnimalExperience === true}
+              onChange={() =>
+                setFormData({ ...formData, hasAnimalExperience: true })
+              }
+            />{" "}
+            Ja
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="hasAnimalExperience"
+              checked={formData.hasAnimalExperience === false}
+              onChange={() =>
+                setFormData({ ...formData, hasAnimalExperience: false })
+              }
+            />{" "}
+            Nej
+          </label>
+        </div>
       </form>
     </main>
   );
