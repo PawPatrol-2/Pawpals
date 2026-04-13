008d55870cbee287508c2bb52e17b188e60c76b8
bjorkmanemelie <bjorkmanemelie@hotmail.com>
2026-04-13 15:07:43 +0200

    route for application

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,16 +1,15 @@
-
-import express from 'express';
-import usersRouter from './routes/users'
-import animalsRouter from './routes/animals'
-import cors from 'cors'
+import express from "express";
+import usersRouter from "./routes/users";
+import animalsRouter from "./routes/animals";
+import cors from "cors";
+import applicationsRouter from "./routes/applications";
 
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
-app.use('/api/users', usersRouter);
-app.use('/api/animals', animalsRouter);
-
+app.use("/api/users", usersRouter);
+app.use("/api/animals", animalsRouter);
+app.use("/api/applications", applicationsRouter);
 
 export default app;
