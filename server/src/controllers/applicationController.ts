325a4138988399c4851448a059a315433826d842
bjorkmanemelie <bjorkmanemelie@hotmail.com>
2026-04-13 14:50:12 +0200

    applicationController

--- server/src/controllers/applicationController.ts
+++ server/src/controllers/applicationController.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { ApplicationResponse, CreateApplicationBody } from "../types";
+import Application from "../models/Application";
 
 export const createApplication = async (
   req: Request<{}, {}, CreateApplicationBody>,
@@ -7,8 +8,22 @@ export const createApplication = async (
   next: NextFunction,
 ): Promise<void> => {
   try {
-    // logiken här
+    const application = await Application.create(req.body);
+    res.status(201).json(application);
   } catch (error) {
     next(error);
+  }
+};
+
+export const getAllApplications = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const getApplication = await Application.find();
+    res.json(getApplication);
+  } catch (Error) {
+    next(Error);
   }
 };
