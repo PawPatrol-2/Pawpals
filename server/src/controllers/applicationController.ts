0aad2c4a41f8e2354a53a82f596dd618cc38ddb8
JohannesFolkesson <johannes.folkesson@chasacademy.se>
2026-04-16 11:01:12 +0200

    Token för mina adoptioner

--- server/src/controllers/applicationController.ts
+++ server/src/controllers/applicationController.ts
@@ -1,8 +1,21 @@
 import { Request, Response, NextFunction } from "express";
-import { ApplicationResponse, CreateApplicationBody } from "../types";
+import { ApplicationResponse, CreateApplicationBody } from "../types/applicationTypes";
 import Application from "../models/Application";
 import type { AuthenticatedRequest } from "../middleware/auth";
 
+export const getMyApplications = async (req: AuthenticatedRequest, res: Response) => {
+  try {
+    const userId = req.user?.userId
+    if(!userId) {
+      return res.status(400).json({ message: "Användar-ID saknas"})
+    }
+
+    const applications = await Application.find({ userId})
+    res.status(200).json({ applications })
+  } catch (error) {
+    res.status(500).json({ message: "Kunde inte hämta ansökningar", error})
+  }
+}
 export const createApplication = async (
   req: Request<Record<string, never>, unknown, CreateApplicationBody>,
   res: Response<ApplicationResponse>,
