b0b561d65226f88b2f03977353bbc9c1ef41e503
bjorkmanemelie <bjorkmanemelie@hotmail.com>
2026-06-06 21:36:43 +0200

    redesign footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,19 +1,40 @@
-import styles from "../footer/footer.module.css";
-import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import styles from '../footer/footer.module.css';
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 export default function Footer() {
   return (
-    <div className={styles.footer}>
-      <div className={styles.links}>
-        <a href="#">Om oss</a>
-        <a href="#">Kontakta oss</a>
-        <a href="#">GDPR</a>
+    <footer className={styles.footer}>
+      <div className={styles.inner}>
+        <div className={styles.brand}>
+          <div className={styles.logo}>
+            🐾 <span>Paw</span>Pals
+          </div>
+          <p className={styles.tagline}>Vi hjälper djur hitta sitt forever home.</p>
+          <div className={styles.icons}>
+            <FaFacebook />
+            <FaInstagram />
+            <FaTwitter />
+          </div>
+        </div>
+
+        <div className={styles.col}>
+          <h4>Utforska</h4>
+          <a href="/utforska">Alla djur</a>
+          <a href="/organisationer">Organisationer</a>
+          <a href="/mina-ansokningar">Mina ansökningar</a>
+        </div>
+
+        <div className={styles.col}>
+          <h4>Om oss</h4>
+          <a href="#">Om PawPals</a>
+          <a href="#">Kontakta oss</a>
+          <a href="#">GDPR</a>
+        </div>
       </div>
-      <div className={styles.icons}>
-        <FaFacebook />
-        <FaInstagram />
-        <FaTwitter />
+
+      <div className={styles.bottom}>
+        <p>© 2026 PawPals. Alla rättigheter förbehållna.</p>
       </div>
-    </div>
+    </footer>
   );
 }
